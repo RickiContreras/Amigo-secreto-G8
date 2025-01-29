22e1348f2ee125ee5c00e3de43e7148b1eac9c0d
Enrique Contreras <rickiocontreras@gmail.com>
2025-01-29 16:18:24 -0300

    Cambio Titulo  practica 1

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //Crea la lista de amigos. Esta lista debe ser ingresada por el usuario. se debe presentar en pantalla.
 let listaAmigos = []
+
+
+
+//A modo de practica voy a cambiar el texto Amigo Secreto, manipulando el DOM
+let cambioTitulo = document.querySelector('h1')
+    cambioTitulo.innerHTML = 'Amigo Secreto.v1 G8';
+    
