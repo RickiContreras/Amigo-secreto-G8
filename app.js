0dccf8686e3d4aea7c00834a5b65f8acb0f5f42c
Enrique Contreras <rickiocontreras@gmail.com>
2025-01-29 08:54:08 -0300

    Declara Array listaAmigos

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Crea la lista de amigos. Esta lista debe ser ingresada por el usuario. se debe presentar en pantalla.
+let listaAmigos = []
